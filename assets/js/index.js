9b682441f832b4efce6ef9b2547560b1d1184655
tiff8433 <tiffany.l.huang@gmail.com>
2016-01-25 21:07:38 -0800

    initial commit

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,55 +2,30 @@
  * Main JS file for Casper behaviours
  */
 
-/* globals jQuery, document */
-(function ($, undefined) {
+/*globals jQuery, document */
+(function ($) {
     "use strict";
+    $(document).ready(function(){
 
-    var $document = $(document);
+		// Taken from http://css-tricks.com/snippets/jquery/smooth-scrolling/
+		$('a[href*=#]:not([href=#])').click(function() {
+			if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+				var target = $(this.hash);
+				target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+				if (target.length) {
+					$('html,body').animate({
+						scrollTop: (target.offset().top - 50)
+					}, 739);
+					return false;
+				}
+			}
+		});
 
-    $document.ready(function () {
+        // On the home page, move the blog icon inside the header 
+        // for better relative/absolute positioning.
 
-        var $postContent = $(".post-content");
-        $postContent.fitVids();
-
-        $(".scroll-down").arctic_scroll();
-
-        $(".menu-button, .nav-cover, .nav-close").on("click", function(e){
-            e.preventDefault();
-            $("body").toggleClass("nav-opened nav-closed");
-        });
+        //$("#blog-logo").prependTo("#site-head-content");
 
     });
 
-    // Arctic Scroll by Paul Adam Davis
-    // https://github.com/PaulAdamDavis/Arctic-Scroll
-    $.fn.arctic_scroll = function (options) {
-
-        var defaults = {
-            elem: $(this),
-            speed: 500
-        },
-
-        allOptions = $.extend(defaults, options);
-
-        allOptions.elem.click(function (event) {
-            event.preventDefault();
-            var $this = $(this),
-                $htmlBody = $('html, body'),
-                offset = ($this.attr('data-offset')) ? $this.attr('data-offset') : false,
-                position = ($this.attr('data-position')) ? $this.attr('data-position') : false,
-                toMove;
-
-            if (offset) {
-                toMove = parseInt(offset);
-                $htmlBody.stop(true, false).animate({scrollTop: ($(this.hash).offset().top + toMove) }, allOptions.speed);
-            } else if (position) {
-                toMove = parseInt(position);
-                $htmlBody.stop(true, false).animate({scrollTop: toMove }, allOptions.speed);
-            } else {
-                $htmlBody.stop(true, false).animate({scrollTop: ($(this.hash).offset().top) }, allOptions.speed);
-            }
-        });
-
-    };
-})(jQuery);
+}(jQuery));
